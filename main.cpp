64f561de61309452c4e4e681ae5c0c4dd7db3cd9
CuinnKemp <cuinn.kemp@student.adelaide.edu.au>
2024-03-27 16:53:34 +1030

    maybe maybe?

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    string s1 = "4634254567", s2 = "45689";
+    string s1 = "1111", s2 = "112111";
     Finder f1;
     vector<int> res = f1.findSubstrings(s1, s2);
     for (auto i : res){
