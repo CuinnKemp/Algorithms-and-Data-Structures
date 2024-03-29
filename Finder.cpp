71c84b7d5543efb9cb06693990f6765c00a04fff
CuinnKemp <cuinn.kemp@student.adelaide.edu.au>
2024-03-29 18:12:16 +1030

    even better KMP

--- Finder.cpp
+++ Finder.cpp
@@ -35,51 +35,42 @@ using namespace std;
 #include <unordered_map>
 
 vector<int> Finder::findSubstrings(string& s1, string& s2) {
-    // init result vector
-    vector<int> result;
-    
     int n = s1.size(), m = s2.size();
 
-    // complexity is O()
-    int start_from = 0;
-    for (int i = 1; i <= s2.size(); i++){
-        string pattern = s2.substr(0, i);
-        result.push_back(KMP(s1, pattern, start_from));
-        if (result.back() == -1){
-            break;
-        } else {
-            start_from = result.back();
-        }
-    }
+    vector<int> pattern_index(m + 1, 0);
 
-
-    return result;
-}
-
-int Finder::KMP(string& text, string& pattern, int start = 0){
-    vector<int> pattern_index(pattern.size() + 1, 0);
-
+    {
     unordered_map<char, int> last_occurrence;
-    for (int i = 1; i <= pattern.size(); i++){
-        if(last_occurrence.find(pattern[i - 1]) != last_occurrence.end()) {
-            pattern_index[i] = last_occurrence[pattern[i - 1]];
+    for (int i = 1; i <= m; i++){
+        if(last_occurrence.find(s2[i - 1]) != last_occurrence.end()) {
+            pattern_index[i] = last_occurrence[s2[i - 1]];
         }
-        last_occurrence[pattern[i - 1]] = i;
+        last_occurrence[s2[i - 1]] = i;
     }
+    }
+
+    // init result vector
+    vector<int> result;
 
     int j = 0;
-    for (int i = start; i < text.size(); i++){
+    for (int i = 0; i < s1.size(); i++){
         // find last character than matches the current text[i] value
-        while (j > 0 && text[i] != pattern[j]){
+        while (j > 0 && s1[i] != s2[j]){
             j = pattern_index[j];
         }
-        if (text[i] == pattern[j]){
+        if (s1[i] == s2[j]){
+            if (result.size() == j){
+                result.push_back(i - j);
+            }
             j++;
         }
-        if (j == pattern.size()){
-            return i - j + 1; // Returning the index of the first occurrence
+        if (j == s2.size()){
+            break; // Returning the index of the first occurrence
         }
     }
-    return -1; // Not found
+    if (result.size() != s2.size()){
+        result.push_back(-1);
+    }
+    return result;
 }
 
--- Finder.h
+++ Finder.h
@@ -5,8 +5,6 @@
 #include <string>
 #include <vector>
 class Finder {
-private:
-    int KMP(std::string& text, std::string& pattern, int start);
 public:
     std::vector<int> findSubstrings(std::string& s1, std::string& s2);
 };
