1196ef8216f6ab9691d8591322502e1c4bb089cf
CuinnKemp <cuinn.kemp@student.adelaide.edu.au>
2024-03-29 18:23:42 +1030

    adding comments xoxo <3

--- Finder.cpp
+++ Finder.cpp
@@ -1,76 +1,97 @@
-// #include "Finder.h"
-// using namespace std;
-// vector<int> Finder::findSubstrings(string& s1, string& s2) {
-//     // init result vector
-//     vector<int> result;
+/*
+// this is the modification of the file provided 
 
-//     // for loop for range 1->n s1
-//     for (size_t i = 1; i <= s2.size(); i++) {
-//         // get index of the substring occurance in s2
-//         size_t found = string::npos;
-//         if (result.size() > 0){
-//             found = s1.find(s2.substr(0, i), (size_t)result.back()); // o(len(s1)*len(s2)) opperation
-//         } else {
-//             found = s1.find(s2.substr(0, i)); // o(len(s1)*len(s2)) opperation
-//         }
+#include "Finder.h"
+using namespace std;
+vector<int> Finder::findSubstrings(string& s1, string& s2) {
+    // init result vector
+    vector<int> result;
 
-//         // if the substring is in s1
-//         if (found != string::npos) {
-//             // append the index to the result vector
-//             result.push_back(found);
-//         } else {
-//             // append -1 to the result vector
-//             result.push_back(-1);
-//             break;
-//         }
-//     }
-//     return result;
-// }
+    // for loop for range 1->n s1
+    for (size_t i = 1; i <= s2.size(); i++) {
+        // get index of the substring occurance in s2
+        size_t found = string::npos;
+        if (result.size() > 0){
+            found = s1.find(s2.substr(0, i), (size_t)result.back()); // o(len(s1)*len(s2)) opperation
+        } else {
+            found = s1.find(s2.substr(0, i)); // o(len(s1)*len(s2)) opperation
+        }
 
+        // if the substring is in s1
+        if (found != string::npos) {
+            // append the index to the result vector
+            result.push_back(found);
+        } else {
+            // append -1 to the result vector
+            result.push_back(-1);
+            break;
+        }
+    }
+    return result;
+}
+*/
 
-
+// this is a far more efficient solution 
 #include "Finder.h"
 using namespace std;
 
 #include <unordered_map>
 
+// function time complexity is O(n*m) n = length of s1 and m = length of s2
 vector<int> Finder::findSubstrings(string& s1, string& s2) {
+    // n is the size of s1 and m is the size of s2
+    // this was mainly so the Big O notation was easy to understand
     int n = s1.size(), m = s2.size();
 
     vector<int> pattern_index(m + 1, 0);
 
+    // declare a scope (so that the last_occurence map doesnt sit in memory)
     {
-    unordered_map<char, int> last_occurrence;
-    for (int i = 1; i <= m; i++){
-        if(last_occurrence.find(s2[i - 1]) != last_occurrence.end()) {
-            pattern_index[i] = last_occurrence[s2[i - 1]];
+        // create the pattern_index table O(m)
+        unordered_map<char, int> last_occurrence;
+        for (int i = 1; i <= m; i++){
+            if(last_occurrence.find(s2[i - 1]) != last_occurrence.end()) {
+                pattern_index[i] = last_occurrence[s2[i - 1]];
+            }
+            last_occurrence[s2[i - 1]] = i;
         }
-        last_occurrence[s2[i - 1]] = i;
-    }
     }
 
     // init result vector
     vector<int> result;
 
+    // conduct the pattern matching algorithm and store the first time a new value of j is reached (create result)
+    // time complexity O(n)
+
     int j = 0;
-    for (int i = 0; i < s1.size(); i++){
-        // find last character than matches the current text[i] value
+    for (int i = 0; i < n; i++){
+        // find last character that matches the current text[i] value
         while (j > 0 && s1[i] != s2[j]){
             j = pattern_index[j];
         }
+
+        // if there is a match 
         if (s1[i] == s2[j]){
-            if (result.size() == j){
+            // check if this is the first time a match has been seen with this value of j
+            if ((int)result.size() == j){
+                // if it is the start of the matched patern to the result vector
                 result.push_back(i - j);
             }
+            // increment j
             j++;
         }
-        if (j == s2.size()){
+        // if j has reached the end of string s2
+        if (j == m){
             break; // Returning the index of the first occurrence
         }
     }
+
+    // if the pattern was not completely matched then add the -1 to indicate this
     if (result.size() != s2.size()){
         result.push_back(-1);
     }
+
+    // return the result vector
     return result;
 }
 
