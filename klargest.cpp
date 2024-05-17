5394b833c71875c7f072bce3ec4a342004a6662f
CuinnKemp <cuinn.kemp@student.adelaide.edu.au>
2024-05-17 16:25:39 +0930

    yippie

--- klargest.cpp
+++ klargest.cpp
@@ -5,15 +5,17 @@ int kth_largest(std::vector<int> values, int k){
     std::priority_queue<int> pq;
 
     for (auto i : values){
-        pq.push(i);
-    }
-
-    while (!pq.empty() && k > 0){
-        pq.pop();
-        k--;
-        if (k == 0){
-            return pq.top();
+        if (pq.size() >= k){
+            if (-pq.top() < i){
+                pq.push(-i);
+                pq.pop();
+            }
+        } else {
+            pq.push(-i);
         }
     }
+    if (pq.size() == k)
+        return -pq.top();
+
     return -1;
 }
--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
 int main(){
     
-    cout << kth_largest({10,9,8,7,6,5,4,3,2,1}, 1) << endl;
+    cout << kth_largest({10,9,8,7,6,5,4,3,2,1}, 3) << endl;
     
 }
